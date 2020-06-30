a38dc7ec8aa9f17a69e6edc74e2d319bf22bb285
olajyde <olamichael.ade@gmail.com>
2020-06-30 21:40:00 +0100

    Reusable ImageInput component

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
 import Screen from './app/components/Screen';
-import AppTextInput from './app/components/AppTextInput';
+import ImageInput from './app/components/ImageInput';
 
 export default function App() {
+  const [imageUri, setImageUri] = useState();
+
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (!granted) alert('You need to enable permissions to access the library');
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
   return (
     <Screen>
-      <AppTextInput placeholder='Username' icon='email' />
+      <ImageInput
+        imageUri={imageUri}
+        onChangeImage={(uri) => setImageUri(uri)}
+      />
     </Screen>
   );
 }
---
+++ app/components/ImageInput.js
@@ -0,0 +1,76 @@
+import React, { useEffect } from 'react';
+import {
+  View,
+  StyleSheet,
+  Image,
+  TouchableWithoutFeedback,
+  Alert,
+} from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker';
+import colors from '../config/colors';
+
+const ImageInput = ({ imageUri, onChangeImage }) => {
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (!granted) alert('You need to enable permissions to access the library');
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const handlePress = () => {
+    if (!imageUri) selectImage();
+    else
+      Alert.alert('Delete', 'Are you sure you want to delete this image?', [
+        { text: 'Yes', onPress: () => onChangeImage(null) },
+        { text: 'No' },
+      ]);
+  };
+
+  const selectImage = async () => {
+    try {
+      const res = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 0.7,
+      });
+
+      if (!res.cancelled) {
+        onChangeImage(res.uri);
+      }
+    } catch (error) {
+      console.log('Error selecting an image');
+    }
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={handlePress}>
+      <View style={styles.container}>
+        {!imageUri && (
+          <MaterialCommunityIcons name='camera' color={colors.dark} size={40} />
+        )}
+        {imageUri && <Image style={styles.image} source={{ uri: imageUri }} />}
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: 100,
+    height: 100,
+    borderRadius: 15,
+    backgroundColor: colors.lightGrey,
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
+
+export default ImageInput;
