0426d9c9a019c24c8e400f429febfed525b0ecee
olajyde <olamichael.ade@gmail.com>
2020-07-01 11:44:27 +0100

    Added auth navigator

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { ImageBackground, StyleSheet, View, Image, Text } from 'react-native';
-import AppButton from '../components/Button';
+import Button from '../components/Button';
 
-const WelcomeScreen = () => {
+const WelcomeScreen = ({ navigation }) => {
   return (
     <ImageBackground
       blurRadius={10}
@@ -14,8 +14,12 @@ const WelcomeScreen = () => {
         <Text style={styles.tagLine}>Sell what you don't need</Text>
       </View>
       <View style={styles.buttonContainer}>
-        <AppButton title='Login' />
-        <AppButton title='Register' color='secondary' />
+        <Button title='Login' onPress={() => navigation.navigate('Login')} />
+        <Button
+          title='Register'
+          color='secondary'
+          onPress={() => navigation.navigate('Register')}
+        />
       </View>
     </ImageBackground>
   );
