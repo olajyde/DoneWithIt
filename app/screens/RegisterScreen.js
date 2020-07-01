4d4250701dc982a498a9b13905533bb4375e8d63
olajyde <olamichael.ade@gmail.com>
2020-07-01 11:43:18 +0100

    Added Register screen component

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,59 +1,58 @@
 import React from 'react';
-import { StyleSheet, Image, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import * as Yup from 'yup';
 
 import Screen from '../components/Screen';
 import { Form, FormField, SubmitButton } from '../components/form';
 
 const validationSchema = Yup.object().shape({
+  name: Yup.string().required().label('Name'),
   email: Yup.string().required().email().label('Email'),
   password: Yup.string().required().min(4).label('Password'),
 });
 
-const LoginScreen = () => {
+function RegisterScreen() {
   return (
     <Screen style={styles.container}>
-      <Image style={styles.logo} source={require('../assets/logo-red.png')} />
       <Form
-        initialValues={{ email: '', password: '' }}
+        initialValues={{ name: '', email: '', password: '' }}
         onSubmit={(values) => console.log(values)}
         validationSchema={validationSchema}
       >
         <FormField
-          name='email'
+          autoCorrect={false}
+          icon='account'
+          name='name'
+          placeholder='Name'
+        />
+        <FormField
           autoCapitalize='none'
           autoCorrect={false}
           icon='email'
           keyboardType='email-address'
-          textContentType='emailAddress'
+          name='email'
           placeholder='Email'
+          textContentType='emailAddress'
         />
         <FormField
-          name='password'
           autoCapitalize='none'
           autoCorrect={false}
           icon='lock'
-          secureTextEntry
+          name='password'
           placeholder='Password'
+          secureTextEntry
           textContentType='password'
         />
-        <SubmitButton title='Login' />
+        <SubmitButton title='Register' />
       </Form>
     </Screen>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     padding: 10,
   },
-  logo: {
-    width: 80,
-    height: 80,
-    alignSelf: 'center',
-    marginTop: 50,
-    marginBottom: 20,
-  },
 });
 
-export default LoginScreen;
+export default RegisterScreen;
